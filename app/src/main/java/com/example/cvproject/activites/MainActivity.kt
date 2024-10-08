ef9e95e3f3806cc533250915f999e08e57e3b2b0
Aarif Saifi <mohdaarifdhara@gmail.com>
2024-10-08 19:24:08 +0530

    enable viewbinding in project.

--- app/src/main/java/com/example/cvproject/activites/MainActivity.kt
+++ app/src/main/java/com/example/cvproject/activites/MainActivity.kt
@@ -1,4 +1,14 @@
 package com.example.cvproject.activites
 
-class MainActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cvproject.databinding.MainActivityBinding
+
+class MainActivity:AppCompatActivity() {
+    private lateinit var binding: MainActivityBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 }
