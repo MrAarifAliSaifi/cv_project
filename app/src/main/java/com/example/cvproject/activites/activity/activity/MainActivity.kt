b871b52786e5e32d5303addecf62b687a5e744ba
ashishmohan1234 <ashishmohan@edugorilla.org>
2024-10-19 18:30:15 +0530

    Remove no internet check.

--- app/src/main/java/com/example/cvproject/activites/activity/activity/MainActivity.kt
+++ app/src/main/java/com/example/cvproject/activites/activity/activity/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.Intent
 import android.view.View
 import androidx.activity.viewModels
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.cvproject.activites.activity.base.BaseActivity
@@ -13,8 +12,6 @@ import com.example.cvproject.activites.activity.viewmodeles.MainActivityVM
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import cvproject.blinkit.R
 import cvproject.blinkit.databinding.ActivityMainBinding
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class MainActivity : BaseActivity<ActivityMainBinding, MainActivityVM>() {
     private val viewmodel: MainActivityVM by viewModels()
@@ -38,15 +35,6 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainActivityVM>() {
 
     override fun setupUI() {
         setupNavigation()
-        if (Utils.isInternetConnected(this@MainActivity)) {
-            lifecycleScope.launch {
-                binding.internet.root.setBackgroundColor(R.color.green_1B7938)
-                delay(2000)
-                binding.internet.root.visibility = View.GONE
-            }
-        } else {
-            binding.internet.root.visibility = View.VISIBLE
-        }
     }
 
     override fun setupListeners() {
