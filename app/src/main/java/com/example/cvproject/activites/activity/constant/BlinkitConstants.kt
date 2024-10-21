0d27cdb8e4a03326c0a6dea79c469e416b4ef475
ashishmohan1234 <ashishmohan@edugorilla.org>
2024-10-21 20:49:54 +0530

    Added missing file.

--- app/src/main/java/com/example/cvproject/activites/activity/constant/BlinkitConstants.kt
+++ app/src/main/java/com/example/cvproject/activites/activity/constant/BlinkitConstants.kt
@@ -3,5 +3,8 @@ package com.example.cvproject.activites.activity.constant
 object BlinkitConstants {
     const val LOCATION = "location"
     const val IS_LOGGED_IN = "isLoggedIn"
+    const val IS_ITEM_ADDED = "isItemAdded"
+    const val SELECTED_ITEM_DETAILS = "selectedItemDetails"
+    const val SELECTED_ITEM_IMAGE_URL = "selectedItemImageUrl"
     const val Verification_ID = "verification_Id"
 }
